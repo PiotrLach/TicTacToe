3b0903e0bcee2a5fd68044f2135c62ffaa57b41f
Piotr Lach <mcmoher59@gmail.com>
2021-12-10 16:52:07 +0100

    Reformatted code

--- src/Game.jsx
+++ src/Game.jsx
@@ -7,11 +7,15 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>
+            {/* status */}
+          </div>
+          <ol>
+            {/* TODO */}
+          </ol>
         </div>
       </div>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './Game.jsx';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+var gameIndex = {
+    root : document.getElementById('root')
+}
+
+ReactDOM.render(<Game />, gameIndex.root);
