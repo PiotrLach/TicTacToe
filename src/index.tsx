d3a835ba9d25cce5d2b97a540ce5cd246aac160b
Piotr Lach <mcmoher59@gmail.com>
2021-12-25 23:48:47 +0100

    Removed redundant code

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './Game';
+import Board from './Board';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<Game />, root);
+ReactDOM.render(<Board />, root);
