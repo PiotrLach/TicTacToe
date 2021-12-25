6db6f892fa236ec2765660e6d4bfa972690dbbc9
Piotr Lach <mcmoher59@gmail.com>
2021-12-25 18:51:42 +0100

    Added test for VictoryChecker class

--- src/VictoryChecker.ts
+++ src/VictoryChecker.ts
@@ -1,3 +1,5 @@
+type Coords = [number, number, number, number];
+
 export default class VictoryChecker {
 
     private readonly squares;
@@ -5,34 +7,45 @@ export default class VictoryChecker {
     private readonly DIR_HORIZONTAL = 0;
     private readonly DIR_VERTICAL = 1;
 
-    constructor (squares: Array<string>) {
-        this.squares = squares;        
+    constructor(squares: Array<string>) {
+        this.squares = squares;
     }
 
-    isGameWon(): boolean {
-        return this.checkRows() || this.checkCols() || this.checkDiagonals();
+    public isGameOver(): boolean {
+        return this.isAnyRowUniform() || this.isAnyColUniform() || this.isAnyDiagonalUniform();
     }
 
-    private checkRows(): boolean {
-        return this.isGameOver(this.DIR_HORIZONTAL);
+    private isAnyRowUniform(): boolean {
+        return this.isRowOrColUniform(this.DIR_HORIZONTAL);
     }
 
-    private checkCols(): boolean {   
-        return this.isGameOver(this.DIR_VERTICAL);
+    private isAnyColUniform(): boolean {
+        return this.isRowOrColUniform(this.DIR_VERTICAL);
     }
 
-    private checkDiagonals(): boolean {   
-        return this.checkDiagonal();
+    private isAnyDiagonalUniform(): boolean {
+        const start = 0;
+        const end = this.DIMENS - 1;
+
+        const bottomToTop: Coords = [end, start, start, end];
+        const topToBottom: Coords = [start, start, end, end];
+
+        const opt1 = this.isDiagonalUniform(bottomToTop);
+        const opt2 = this.isDiagonalUniform(topToBottom);
+
+        return opt1 || opt2;
     }
 
-    private checkDiagonal(): boolean {   
+    private isDiagonalUniform(coords: Coords): boolean {
+
+        const [x1, y1, x2, y2] = coords;
 
         const excerpt = [];
 
         let i = 0, j = 0;
 
         while (i != this.DIMENS || j != this.DIMENS) {
-            
+
             const index = i * this.DIMENS + j;
             const symbol = this.squares[index];
             excerpt.push(symbol);
@@ -44,13 +57,13 @@ export default class VictoryChecker {
         return this.isExcerptUniform(excerpt);
     }
 
-    private isGameOver(mode: number): boolean {
+    private isRowOrColUniform(mode: number): boolean {
         for (let i = 0; i < this.DIMENS; i++) {
-            
+
             const excerpt = [];
-            
+
             for (let j = 0; j < this.DIMENS; j++) {
-                
+
                 const direction = [
                     i * this.DIMENS + j,
                     j * this.DIMENS + i
@@ -63,12 +76,12 @@ export default class VictoryChecker {
 
             if (this.isExcerptUniform(excerpt)) {
                 return true;
-            }            
+            }
         }
         return false;
     }
 
-    private isExcerptUniform(excerpt: Array<string>): boolean {      
+    private isExcerptUniform(excerpt: Array<string>): boolean {
 
         const first = excerpt[0];
 
---
+++ src/__tests__/VictoryChecker.test.ts
@@ -0,0 +1,26 @@
+import VictoryChecker from "../VictoryChecker";
+
+const testCases = [
+    ['X', '', '',
+    'O', 'X', '',
+    'O', 'O', 'X'],
+    
+    ['X', 'X', 'X',
+    '', 'O', '',
+    'O', 'O', ''],
+
+    ['X', '', 'O',
+    'X', 'O', '',
+    'X', 'O', '']
+];
+
+for (const testCase of testCases) {
+    
+    const victoryChecker = new VictoryChecker(testCase);
+    const checkIsGameWon = () => {
+        expect(victoryChecker.isGameOver()).toEqual(true);
+    };
+    
+    test('Checks victory', checkIsGameWon);
+}
+
